76c1b9c47fc218eab3ac2476ba8eed6101709133
sungnyun <ksn4397@gmail.com>
2020-07-24 08:27:09 +0000

    no print Time & Data

--- main_advmoco.py
+++ main_advmoco.py
@@ -304,14 +304,14 @@ def main_worker(gpu, ngpus_per_node, args):
 
 
 def train(train_loader, model, generator, criterion, optimizer, optimizer_g, epoch, args):
-    batch_time = AverageMeter('Time', ':6.3f')
-    data_time = AverageMeter('Data', ':6.3f')
+    #batch_time = AverageMeter('Time', ':6.3f')
+    #data_time = AverageMeter('Data', ':6.3f')
     losses = AverageMeter('Loss', ':.4e')
     top1 = AverageMeter('Acc@1', ':6.2f')
     top5 = AverageMeter('Acc@5', ':6.2f')
     progress = ProgressMeter(
         len(train_loader),
-        [batch_time, data_time, losses, top1, top5],
+        [losses, top1, top5],
         prefix="Epoch: [{}]".format(epoch))
 
     # switch to train mode
